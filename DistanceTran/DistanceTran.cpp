94c20669b35110ea66edfe22becbf4fafea91e94
wecol <xunqiduan@qq.com>
2013-12-08 19:38:19 +0800

    firstDemo

--- DistanceTran/DistanceTran.cpp
+++ DistanceTran/DistanceTran.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2\opencv.hpp> 
+using namespace cv;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	IplImage *src;
+	src = cvLoadImage("cornfield.bmp");
+	cvNamedWindow("lena",CV_WINDOW_AUTOSIZE );
+	cvShowImage("lena",src);
+	cvWaitKey(0);
+	cvDestroyWindow("lena");
+	cvReleaseImage(&src);
 	return 0;
 }
 
